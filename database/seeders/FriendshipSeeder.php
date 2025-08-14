92e6f41c548b3614497cd12d27821e9a04dfd55d
diogogoncalves <dmgdark95@gmail.com>
2025-08-14 19:26:59 +0100

    Seeders

--- database/seeders/FriendshipSeeder.php
+++ database/seeders/FriendshipSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Friendship;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class FriendshipSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Friendship::factory(5)->create();
     }
 }

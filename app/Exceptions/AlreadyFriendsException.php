5e39ab809105a0747b850a5d5bc913a2b7d0d41a
diogogoncalves <dmgdark95@gmail.com>
2025-08-16 21:57:19 +0100

    Exception Friendship already existing between users.

--- app/Exceptions/AlreadyFriendsException.php
+++ app/Exceptions/AlreadyFriendsException.php
@@ -6,6 +6,11 @@ use Exception;
 
 class AlreadyFriendsException extends Exception
 {
-    protected $message = 'Já são amigos';
+    protected $message;
     protected $code = 500;
+
+    public function __construct()
+    {
+        parent::__construct(__('exceptions.alreadyFriendsException'), $this->code);
+    }
 }

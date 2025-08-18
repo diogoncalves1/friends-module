aadea987f30a7f8f81e573a6b88462ea181c473c
diogogoncalves <dmgdark95@gmail.com>
2025-08-18 09:16:15 +0100

    Exception: FriendRequestNotFoundException

--- app/Exceptions/FriendRequestNotFoundException.php
+++ app/Exceptions/FriendRequestNotFoundException.php
@@ -6,6 +6,11 @@ use Exception;
 
 class FriendRequestNotFoundException extends Exception
 {
-    protected $message = 'Pedido nao encontrado';
+    protected $message;
     protected $code = 404;
+
+    public function __construct()
+    {
+        parent::__construct(__('exceptions.friendRequestNotFoundException'), $this->code);
+    }
 }

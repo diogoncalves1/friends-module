eace92ea075512b20c3688a8f2ba8c4443a8aecd
diogogoncalves <dmgdark95@gmail.com>
2025-08-19 13:23:02 +0100

    Exception SelfFriendshipException

--- app/Exceptions/SelfFriendshipException.php
+++ app/Exceptions/SelfFriendshipException.php
@@ -6,6 +6,11 @@ use Exception;
 
 class SelfFriendshipException extends Exception
 {
-    protected $message = 'Impossivel enviar pedido para si mesmo';
+    protected $message;
     protected $code = 500;
+
+    public function __construct()
+    {
+        parent::__construct(__('exceptions.selfFriendshipException'), $this->code);
+    }
 }

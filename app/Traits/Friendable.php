1daf49d3a0219cae8213104499768d104583105c
diogogoncalves <dmgdark95@gmail.com>
2025-08-20 09:14:37 +0100

    Friendable Trait

--- app/Traits/Friendable.php
+++ app/Traits/Friendable.php
@@ -6,20 +6,24 @@ use App\Repositories\FriendshipRepository;
 
 trait Friendable
 {
+    public function sendFriendRequest(string $id)
+    {
+        return app(FriendshipRepository::class)->send($id);
+    }
 
     public function acceptFriendRequest(string $id)
     {
         return app(FriendshipRepository::class)->accept($id);
     }
 
-    public function sendFriendRequest(string $id)
-    {
-        return app(FriendshipRepository::class)->send($id);
-    }
-
     public function declineFriendRequest(string $id)
     {
         return app(FriendshipRepository::class)->decline($id);
+    }
+
+    public function removeRelation(string $id)
+    {
+        return app(FriendshipRepository::class)->remove($id);
     }
 
     public function blockUser(string $id)

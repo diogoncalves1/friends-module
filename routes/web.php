21b6254cac0e9886cd194070164c18a5c081ca1b
diogogoncalves <d.m.goncalves640@gmail.com>
2025-11-05 16:03:16 +0000

    Add Api Auth

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::group(
-    [
-        'as' => 'api.',
-        'prefix' => 'api/',
-        // 'middleware' => 'auth'
-    ],
-    function () {
-
-        Route::group([
-            'as' => 'friendships.',
-            'prefix' => 'friendships'
-        ], function () {
-            Route::post('/send/{id}', [\App\Http\Controllers\Api\FriendshipController::class, 'send']);
-            Route::post('/{id}/accept', [\App\Http\Controllers\Api\FriendshipController::class, 'accept'])->name('accept');
-            Route::post('/{id}/decline', [\App\Http\Controllers\Api\FriendshipController::class, 'decline']);
-            Route::delete('/{id}', [\App\Http\Controllers\Api\FriendshipController::class, 'remove']);
-            Route::post('/{id}/block', [\App\Http\Controllers\Api\FriendshipController::class, 'block']);
-            Route::get('/', [\App\Http\Controllers\Api\FriendshipController::class, 'listFriends']);
-            Route::get('/pending', [\App\Http\Controllers\Api\FriendshipController::class, 'listPending']);
-        });
-    }
-);
-
-Route::group([
-    'as' => 'friendships.',
-    'prefix' => 'friendships',
-    // 'middleware' => ['auth']
-], function () {
-    Route::get('/', [\App\Http\Controllers\FriendshipController::class, 'index'])->name('index');
-    Route::get('/pending', [\App\Http\Controllers\FriendshipController::class, 'pending'])->name('pending');
-});

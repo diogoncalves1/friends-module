815ad7bbb2b8d3a1b54b552a6e71e5fd6d580ac4
diogogoncalves <d.m.goncalves640@gmail.com>
2025-11-11 21:15:09 +0000

    Friend Module Update Translate

---
+++ Modules/Friends/Resources/lang/pt/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'friendship-requests' => [
+        'send' => 'Solicitação de amizade enviada com sucesso para :name.',
+        'accept' => 'Solicitação de amizade de :name aceita com sucesso.',
+        'decline' => 'Solicitação de amizade de :name rejeitada com sucesso.',
+        'errors' => [
+            'send' => 'Erro ao enviar solicitação de amizade.',
+            'accept' => 'Erro ao tentar aceitar a solicitação de amizade.',
+            'decline' => 'Erro ao tentar rejeitar a solicitação de amizade.'
+        ]
+    ],
+
+    'friendships' => [
+        'remove' => 'Amizade encerrada com sucesso.',
+        'block' => ':name bloqueado(a) com sucesso.',
+        'unblock' => ':name desbloqueado(a) com sucesso.',
+        'errors' => [
+            'remove' => 'Erro ao tentar encerrar a amizade.',
+            'block' => 'Erro ao tentar bloquear o usuário.',
+            'unblock' => 'Erro ao tentar desbloquear o usuário.'
+        ]
+    ]
+];
--- Modules/Friends/Resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-friends::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('friends.name') !!}</p>
-</x-friends::layouts.master>
